686494d4ea7a3370cb0af13e748fe372cef0c726
Om Mehta <ommehta16@icloud.com>
2025-05-27 10:55:18 -0400

    start

--- Set.java
+++ Set.java
@@ -1,5 +1,7 @@
 public class Set {
     public static void main(String[] args) {
-        
+        StdDraw.setXscale(-1.0, 1.0);
+        StdDraw.setYscale(-1.0, 1.0);
+        StdDraw.enableDoubleBuffering();
     }
 }
